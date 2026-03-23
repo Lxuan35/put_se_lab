39f5e5d2b02fb9a7e6b31f3cd6f90681668c0f5f
louhamon <louan.hamon@etudiant.univ-rennes1.fr>
2026-03-23 17:00:11 +0100

    First change to Program.java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,8 +1,10 @@
+// Java Class printing Hello World
 public class Program {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello World");
             System.out.println("-----------");
+            System.out.println("log : modif")
             System.out.println("Iteration: " + i);
         }
     }
